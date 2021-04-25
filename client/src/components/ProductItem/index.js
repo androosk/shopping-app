5a4f1e294c8988aecb1845ac51e03706a23fa9c9
androosk <atirpok3@gmail.com>
2021-04-25 12:29:51 -0600

    added indexedDB

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -4,6 +4,7 @@ import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import CartItem from "../CartItem";
+import { idbPromise } from '../../utils/helpers';
 
 
 function ProductItem(item) {
@@ -11,20 +12,24 @@ function ProductItem(item) {
   const { cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-
-    if(itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
-    } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
-    }
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+      if (itemInCart) {
+        dispatch({
+          type: UPDATE_CART_QUANTITY,
+          _id: _id,
+          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        });
+        idbPromise('cart', 'put', {
+          ...itemInCart,
+          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        });
+      } else {
+        dispatch({
+          type: ADD_TO_CART,
+          product: { ...item, purchaseQuantity: 1 }
+        });
+        idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+      }
   };
   const {
     image,
